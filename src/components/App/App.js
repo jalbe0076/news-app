8ebeae0c03b9072db00ca4a301b857fe0a61d69e
Jason Alberto <alberto.jason@gmail.com>
2023-09-12 19:01:06 -0400

    Add responsive styling to navigation header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,12 @@ import { useState, useEffect } from 'react';
 function App() {
  
   return (
-    <>
+    <div className='app'>
       <Navigation />
-      <main className='app' >
+      <main className='main-section' >
         <Headline />
       </main>
-    </>
+    </div>
   )
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import './Navigation.scss';
+import Search from '../Search/Search';
 
 const Navigation = () => {
   const arrowSvg = (
-    <svg className='arrow' stroke='black' height="24" viewBox="0 0 10 80" width="40">
+    <svg className='arrow-icon' stroke='black' height="24" viewBox="0 0 60 80" width="40">
       <line className="line top" x1="10" x2="40" y1="40" y2="15" strokeWidth="8" strokeLinecap="round"/>
-      <line className="line middle" x1="70" x2="10" y1="40" y2="40" strokeWidth="8" strokeLinecap="round" />
+      <line className="line middle" x1="90" x2="10" y1="40" y2="40" strokeWidth="8" strokeLinecap="round" />
       <line className="line bottom" x1="40" x2="10" y1="65" y2="40" strokeWidth="8" strokeLinecap="round"/>
     </svg>
   );
@@ -15,6 +16,7 @@ const Navigation = () => {
         <h1 className='nav-title'>News App</h1>
         {arrowSvg}
       </div>
+      <Search />
     </header>
   )
 
