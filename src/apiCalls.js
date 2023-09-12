1a18097a2007155e555ae59e356fd76fbd7ffaca
Jason Alberto <alberto.jason@gmail.com>
2023-09-11 21:17:15 -0400

    Set up initial fetch function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,1 +1,14 @@
-const 
+export const getNews = async () => {
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=${process.env.NEWS_API_KEY}&country=us&pageSize=10`)
+  const data = await handleErrors(response)
+  return data;
+}
+
+const handleErrors = (response) => {
+  if(response.ok) {
+    return response.json();
+  } else {
+    throw new Error(`HTTP Error: ${response.code} ${response.message}`);
+  }
+}
+
