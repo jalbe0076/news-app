1be46272a1b4205f41c075ba0e3a1881f2440fe5
Jason Alberto <alberto.jason@gmail.com>
2023-09-13 12:00:57 -0400

    Remove console logs and add a random key generator

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -1,8 +1,6 @@
 import './Headline.scss';
 import { useEffect, useState } from 'react';
 
-
-
 const Headline = ({articles}) => {
   if (!articles || articles.length === 0) {
     return <p>No articles available.</p>;
@@ -12,7 +10,7 @@ const Headline = ({articles}) => {
     const {title, urlToImage, description} = article;
 
     return (
-      <article className='main-articles' >
+      <article className='main-articles' key={crypto.randomUUID()} >
         <img className='article-image' src={`${urlToImage}`} />
         <h2 className='article-title'>{title}</h2>
         {description && <p className='article-description' >{description}</p>}
